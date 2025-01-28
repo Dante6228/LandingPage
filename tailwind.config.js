0d08f224d3837a5489d5553f0c7c15ed7132d4d8
Dante <dantealejandro35@gmail.com>
2025-01-28 06:30:54 -0600

    Cambios en producción

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
+  content: ["./**/*.{html,php}"],
   theme: {
     extend: {
       fontFamily: {
